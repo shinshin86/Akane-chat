31277084b9c96852508d01aaeb87025d69031885
shinshin86 <beagles1986@gmail.com>
2016-06-18 11:18:20 +0900

    Create a getInstance and toSay and reply.

--- src/main/akane/Akane.java
+++ src/main/akane/Akane.java
@@ -1,11 +1,43 @@
 package main.akane;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Akane {
-    public Akane(){
-        System.out.println("TODO - Call a Akane");
+    Deque<String> deque;
+    private static Akane instance;
+    
+    public static synchronized Akane getInstance() {
+        if (instance == null) {
+            instance = new Akane();
+            System.out.println("Hash : " + instance.hashCode());
+            instance.deque = new ArrayDeque<String>();
+        }
+        return instance;
     }
     
-    public String toHello(){
-        return "こんにちわ！";
+    // Main Method
+    public String toSay(String word) {
+        // test
+        if(word.equals("こんにちわ")){
+            return toHello();
+        }
+        
+        deque.add(word);
+        try {
+            return reply();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "ERROR!!!!!!!";
+    }
+    
+    public String reply() throws InterruptedException {
+        return deque.poll();
+    }
+    
+    // It's a test method
+    public String toHello() {
+        return "こんにちわ〜なんだか眠くなってきたよ〜";
     }
 }
--- src/test/TestRun.java
+++ src/test/TestRun.java
@@ -1,10 +1,20 @@
 package test;
 
+import java.util.Scanner;
 import main.akane.Akane;
 
 public class TestRun {
+    public static final String exitCommand = "exit";
     public static void main(String[] args){
-        Akane akane = new Akane();
-        System.out.println(akane.toHello());
+        System.out.println("TEST START!!!");
+        Akane akaneInstance = Akane.getInstance();
+        Scanner sc = new Scanner(System.in);
+        String sendWord;
+        
+        do{
+            sendWord = sc.nextLine();
+            System.out.println(akaneInstance.toSay(sendWord));
+        }while(!(str.equals(exitCommand)));
+        System.out.println("Akane is sleeping.....zzz");
     }
 }
